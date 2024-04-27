e3cc16d7903b89a6fda152b5a84ab2cd0dd86411
valerisuleo <valerio.risuleo@gmail.com>
2024-04-27 18:15:12 +0100

    add spinner

--- src/app/views/games/hooks/useGames.tsx
+++ src/app/views/games/hooks/useGames.tsx
@@ -4,19 +4,26 @@ import { gameService } from '../service';
 
 const useGames = () => {
     const [games, setGames] = useState<IGame[]>([]);
+    const [isLoading, setSpinner] = useState(false);
 
     useEffect(() => {
         getGames();
     }, []);
 
     async function getGames(): Promise<void> {
-        const promise = gameService.get('games');
-        const { data } = await promise;
-        setGames(data.results);
-        console.log(data);
+        try {
+            setSpinner(true);
+            const promise = gameService.get('games');
+            const { data } = await promise;
+            setGames(data.results);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setSpinner(false);
+        }
     }
 
-    return { games };
+    return { games, isLoading };
 };
 
 export default useGames;
--- src/app/views/games/list/index.tsx
+++ src/app/views/games/list/index.tsx
@@ -2,10 +2,12 @@ import CardComponent from '../../../common/library/components/cards/card';
 import { cardProps } from '../config';
 import { useTheme } from '../../../common/context/theme/theme';
 import useGames from '../hooks/useGames';
+import { Fragment } from 'react';
+import SpinnerComponent from '../../../common/library/components/spinner/spinner';
 
 const GameIndex = () => {
     const { isDarkMode } = useTheme();
-    const { games } = useGames();
+    const { games, isLoading } = useGames();
 
     return (
         <div>
@@ -14,22 +16,28 @@ const GameIndex = () => {
                 <div className="col-md-2">Side</div>
                 <div className="col-md-10">
                     <div className="row">
-                        {games.map((item) => {
-                            const props = cardProps(item, isDarkMode);
-                            return (
-                                <div
-                                    className="col-md-6 col-lg-4 mb-4"
-                                    key={item.id}
-                                >
-                                    <CardComponent
-                                        header={props.header}
-                                        body={props.body}
-                                        classes={props.classes}
-                                        isDarkMode={props.isDarkMode}
-                                    />
-                                </div>
-                            );
-                        })}
+                        {isLoading ? (
+                            <SpinnerComponent color={'primary'} />
+                        ) : (
+                            <Fragment>
+                                {games.map((item) => {
+                                    const props = cardProps(item, isDarkMode);
+                                    return (
+                                        <div
+                                            className="col-md-6 col-lg-4 mb-4"
+                                            key={item.id}
+                                        >
+                                            <CardComponent
+                                                header={props.header}
+                                                body={props.body}
+                                                classes={props.classes}
+                                                isDarkMode={props.isDarkMode}
+                                            />
+                                        </div>
+                                    );
+                                })}
+                            </Fragment>
+                        )}
                     </div>
                 </div>
             </div>
