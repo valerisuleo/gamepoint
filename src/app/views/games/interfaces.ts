9d52a5954217a83e45c8b0fe27528b29da4d0070
valerisuleo <valerio.risuleo@gmail.com>
2024-04-27 18:03:43 +0100

    rendering game list

---
+++ src/app/views/games/interfaces.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface IGame {
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: IPlatform[];
+    genres: IGenre[];
+}
+
+export interface IGenre {
+    id: number;
+    name: string;
+    background_image: string;
+    games: IGame[];
+}
+
+export interface IPlatform {
+    id: number;
+    name: string;
+    slug: string;
+    games_count?: number;
+    image_background?: string;
+    image?: null | string;
+    year_start?: null | number;
+    year_end?: null | number;
+    games?: IGame[];
+    icon?: any;
+}
+
+export interface IGameHeader {
+    platforms: any[];
+    filters: IGameFilters;
+}
+
+interface IGameFilters {
+    platforms: string;
+    ordering: string;
+}
--- src/app/views/games/list/index.tsx
+++ src/app/views/games/list/index.tsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { IGame } from '../interfaces';
+import { gameService } from '../service';
+import CardComponent from '../../../common/library/components/cards/card';
+import { cardProps } from '../config';
+import { useTheme } from '../../../common/context/theme/theme';
 
 const GameIndex = () => {
-    return <div>GameIndex</div>;
+    const [games, setGames] = useState<IGame[]>([]);
+    const { isDarkMode } = useTheme();
+
+    useEffect(() => {
+        getGames();
+    }, []);
+
+    async function getGames(): Promise<void> {
+        const promise = gameService.get('games');
+        const { data } = await promise;
+        setGames(data.results);
+        console.log(data);
+    }
+
+    return (
+        <div>
+            <h1 className="my-5">GameIndex</h1>
+            <div className="row">
+                <div className="col-md-2">Side</div>
+                <div className="col-md-10">
+                    <div className="row">
+                        {games.map((item) => {
+                            const props = cardProps(item, isDarkMode);
+                            return (
+                                <div
+                                    className="col-md-6 col-lg-4 mb-4"
+                                    key={item.id}
+                                >
+                                    <CardComponent
+                                        header={props.header}
+                                        body={props.body}
+                                        classes={props.classes}
+                                        isDarkMode={props.isDarkMode}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default GameIndex;
