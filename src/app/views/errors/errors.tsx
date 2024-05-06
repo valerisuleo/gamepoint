2e0fb99754aca58c2af3f54c2d3b352e7fad3239
valerisuleo <valerio.risuleo@gmail.com>
2024-05-06 23:09:23 +0100

    add navbar to 404 page

--- src/app/views/errors/errors.tsx
+++ src/app/views/errors/errors.tsx
@@ -1,17 +1,22 @@
 import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 import Notfound from './not-found/notfound';
+import { Fragment } from 'react';
+import Navbar from '../../common/navbar/navbar';
 
 const ErrorPage = () => {
     const error = useRouteError();
 
     return (
-        <div className="m-5">
-            {isRouteErrorResponse(error) ? (
-                <Notfound />
-            ) : (
-                'Something went wrong...'
-            )}
-        </div>
+        <Fragment>
+            <Navbar></Navbar>
+            <div>
+                {isRouteErrorResponse(error) ? (
+                    <Notfound />
+                ) : (
+                    'Something went wrong...'
+                )}
+            </div>
+        </Fragment>
     );
 };
 
